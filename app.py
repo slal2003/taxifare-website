6def23251ec4ac2131231b3a9df39a35194adeb1
slim allani <slal2003@gmail.com>
2022-10-14 15:03:38 +0200

    amend

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from datetime import datetime
 import streamlit as st
+
 
 import numpy as np
 import pandas as pd
 
-import datetime
+import datetime as dt
 import requests
 
 st.markdown("""
@@ -13,16 +13,15 @@ st.markdown("""
 
             """)
 
-pickup_date = st.date_input('Enter pickup date', value = datetime.datetime(2022,10,14,15,22,32) )
-pickup_time = st.time_input('Enter pickup time', value = datetime.datetime(2022,10,14,15,22,32))
+pickup_date = st.date_input('Enter pickup date', value = dt.datetime(2022,10,14,15,22,32) )
+pickup_time = st.time_input('Enter pickup time', value = dt.datetime(2022,10,14,15,22,32))
 pickup_longitude = st.text_input('Enter pickup_longitude', value=(40))
 pickup_latitude = st.text_input('Enter pickup_latitude', value=(73))
-dropoff_longitude = st.text_input('Enter dropoff_longitude' value=(41))
+dropoff_longitude = st.text_input('Enter dropoff_longitude', value=(41))
 dropoff_latitude = st.text_input('Enter dropoff_latitude', value=(41))
 passenger_count = st.text_input('Passenger count', value=(1))
 
-# pickup_datetime = pickup_date + ' ' + pickup_time
-pickup_datetime = '2022-10-14 10:32:23'
+pickup_datetime = f'{pickup_date} {pickup_time}'
 
 url = 'https://taxifare.lewagon.ai/predict'
 
@@ -38,4 +37,5 @@ fare_query_url = url + '?' + query
 
 fare = requests.get(fare_query_url).json()
 fare = fare['fare']
+fare = "${: 0,.2f}".format(fare)
 st.write(f'fare = {fare}')
