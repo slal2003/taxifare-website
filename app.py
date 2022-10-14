c3c9b670af9fdd3cfa87e0853e2a7895cec4bca9
slim allani <slal2003@gmail.com>
2022-10-14 13:15:01 +0200

    added predicted fare

--- app.py
+++ app.py
@@ -17,40 +17,18 @@ dropoff_latitude = st.text_input('Enter dropoff_latitude')
 passenger_count = st.text_input('Passenger count')
 
 
-data_entry = f"""
+collected_data = f"""
 date = {date_time} \n
 pickup_longitude = {pickup_longitude} \n
+etc
 """
 
+st.write(f'we got \n {collected_data}  ')
 
-st.write(f'we got \n {data_entry}  ')
+# fare_query =
 
+# url = 'https://taxifare.lewagon.ai/predict'
 
+fare = "https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2"
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
-
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Outside the form")
-
-
-df = pd.DataFrame({
-    'first column': list(range(1, 11)),
-    'second column': np.arange(10, 101, 10)
-})
-
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
-
-# and used to select the displayed lines
-head_df = df.head(line_count)
-
-head_df
+fare
