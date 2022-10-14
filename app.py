b516edc5fc3bdc189cdf7f838bbd2f3407179fec
slim allani <slal2003@gmail.com>
2022-10-14 13:56:49 +0200

    amend

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 import streamlit as st
 
 import numpy as np
 import pandas as pd
+
+import datetime
+import requests
 
 st.markdown("""
             # This app will provide you with taxi fare prediction
@@ -9,12 +13,17 @@ st.markdown("""
 
             """)
 
-pickup_datetime = st.text_input('Enter date and time')
+pickup_date = st.date_input('Enter date ae', value = datetime.datetime(2022,10,14,15,22,32) )
+pickup_time = st.time_input('Enter  time', value = datetime.datetime(2022,10,14,15,22,32))
 pickup_longitude = st.text_input('Enter pickup_longitude')
 pickup_latitude = st.text_input('Enter pickup_latitude')
 dropoff_longitude = st.text_input('Enter dropoff_longitude')
 dropoff_latitude = st.text_input('Enter dropoff_latitude')
 passenger_count = st.text_input('Passenger count')
+
+pickup_datetime = pickup_date + ' ' + pickup_time
+
+
 
 
 collected_data = f"""
@@ -35,8 +44,12 @@ query = ('pickup_datetime=' + pickup_datetime
          + '&passenger_count=' + passenger_count
          )
 
+
 fare_query_url = url + '?' + query
-fare_query_url
+
+response = requests.get(fare_query_url)
+print(response)
+
 print(fare_query_url)
 
 fare_test = "https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2"
